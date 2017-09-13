aaf1273de6fd9a6d69db9cd080772ddee58f3809
Takashi Hyakutake <thya6769@uni.sydney.edu.au>
2017-09-14 08:29:07 +1000

    Temporary storage

--- question1.py
+++ question1.py
@@ -3,26 +3,28 @@ from collections import defaultdict
 
 non_terminals = {} # dictionary of string as key and value as NonTerminal class
 grammars = defaultdict(list) # multimap of grammars
-
-
-class NonTerminal:
-
-    def __init__(self, val):
-        self.value = val
-        self.first_set = set()
-        self.follow_set = set()
-
-    def __hash__(self):
-        return hash(self.value)
-
-    def __eq__(self, other):
-        return self.value == other.value
+#
+#
+# class NonTerminal:
+#
+#     def __init__(self, val):
+#         self.value = val
+#         self.first_set = []
+#         self.follow_set = []
+#
+#     def __hash__(self):
+#         return hash(self.value)
+#
+#     def __eq__(self, other):
+#         return self.value == other.value
 
 
 class Grammar:
     def __init__(self, left):
         self.lhs = left
         self.rhs = []
+        self.first_set = []
+        self.follow_set = []
 
 
 def main():
@@ -36,6 +38,8 @@ def main():
         non_terminal = NonTerminal(left) # create non_terminal for left symbol
         non_terminals[non_terminal.value] = non_terminal
 
+
+
         grammar = Grammar(left)
 
         for value in right:
@@ -43,49 +47,99 @@ def main():
 
         grammars[left].append(grammar)
 
-    # for grammar in grammars.values():
-    #     print grammar
+    # fill in the initial first_set from the grammar
+    order_first_set()
+    find_first_set()
 
 
-    find_first_base() # find first set for base case
-
-    # recursively find first set
-    for non_terminal in non_terminals.values():
-        if len(non_terminal.first_set) == 0:
-            # concatenate sets
-            non_terminal.first_set |= find_first_recursive(non_terminal)
+    #
+    # # recursively find first set for any grammar with first symbol being non_terminal
+    # for non_terminal in non_terminals.values():
+    #     for grammar in grammars[non_terminal.value]:
+    #         if grammar.rhs[0] in non_terminals:
+    #             # print grammar.lhs + " " + grammar.rhs[0]
+    #             # concatenate sets
+    #             non_terminal.first_set |= find_first_recursive(non_terminal)
 
     # print
+    print "First:"
     for non_terminal in non_terminals.values():
-        print non_terminal.value + " ",
-        print non_terminal.first_set
+        print " " + non_terminal.value + " ->",
+
+        for val in non_terminal.first_set:
+            print " " + val,
+        print ""
 
 
-def find_first_base():
-    for grammar in grammars.values():
-        for val in grammar:
-            symbol = val.rhs[0]  # first symbol on rhs
-            if symbol not in non_terminals.keys():
-                non_terminals[val.lhs].first_set.add(symbol)
+def initial_first_set():
+    for non_terminal in non_terminals.values():
+        for grammar in grammars[non_terminal.value]:
+            for symbol in grammar.rhs:
+                non_terminal.first_set.append(symbol)
+                if symbol not in non_terminals:
+                    break
 
 
-def find_first_recursive(non_terminal):
-    for grammar in grammars[non_terminal.value]:
-        symbols = set()
+def find_first_set():
 
-        for symbol in grammar.rhs:
-            if symbol in non_terminals and symbol != non_terminal.value: # symbol is a non_terminal
+    needs_first_set = []  # list of non_terminals that needs computations of first set
+    for non_terminal in non_terminals.values():
+        needs_first_set.append(non_terminal)
 
-                symbols |= find_first_recursive(non_terminals[symbol])
-                # continue until you reach symbols without epsilon
-                if "epsilon" not in symbols:
-                    return symbols
-                else:
-                    symbols.remove("epsilon")
+    # use a stack and continue until empty
+    while len(needs_first_set) != 0:
+
+        current = needs_first_set.pop()
+
+        first_set = []
+        first_set.append(current.first_set)
+        for symbol in first_set:
+            if symbol in non_terminals:
+                current.first_set.remove(symbol)
+                current.first_set.insert(non_terminals[symbol].first_set, 0)
 
 
-    # we reached the base case thus return the first_set
-    return non_terminal.first_set
+        # check if it contains non_terminals
+        for value in current.first_set:
+            if value in non_terminals:
+                needs_first_set.append(current)
+                break
+
+
+
+# def find_first_base():
+#     for grammar in grammars.values():
+#         for val in grammar:
+#             symbol = val.rhs[0]  # first symbol on rhs
+#             if symbol not in non_terminals.keys():
+#                 non_terminals[val.lhs].first_set.add(symbol)
+#
+#
+# def find_first_recursive(non_terminal):
+#
+#     for grammar in grammars[non_terminal.value]:
+#         if grammar.rhs[0] not in non_terminals:
+#             # print grammar.lhs + " " + grammar.rhs[0]
+#             # print non_terminal.first_set
+#             return non_terminal.first_set
+#
+#         else: # non-terminal
+#             symbols = set()
+#             contains_epsilon = False
+#             for symbol in grammar.rhs:
+#                 if symbol in non_terminals and symbol != non_terminal.value:
+#                     symbols |= find_first_recursive(non_terminals[symbol])
+#                     # continue until you reach symbols without epsilon
+#                     if "epsilon" not in symbols:
+#                         return symbols
+#                     else:
+#                         symbols.remove("epsilon")
+#                         contains_epsilon = True
+#                 else: # symbol is a terminal so add it
+#                     if contains_epsilon:
+#                         symbols.add(symbol)
+#                         return symbols
+
 
 
 if __name__ == '__main__':
