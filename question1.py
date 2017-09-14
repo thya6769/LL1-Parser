54b8743b445ec1c39a2858e0fee6884d75791f99
Takashi Hyakutake <thya6769@uni.sydney.edu.au>
2017-09-14 20:47:41 +1000

    Started implementing follow set

--- question1.py
+++ question1.py
@@ -47,26 +47,42 @@ def main():
         if len(v.first_set) == 0:
             find_first_sets(v)
 
+    list(non_terminals.items())[0][1].follow_set.add("$")
+    for v in non_terminals.values():
+        find_follow_sets(v)
 
 
+    sort_sets()
 
-    # reorder the first_set in alphabetical order and epsilon at the end
+    # print
+    print_sets("First")
+    print_sets("Follow")
+
+# reorder the first_set and follow_set in alphabetical order and epsilon at the end
+def sort_sets():
     for non_terminal in non_terminals.values():
         non_terminal.first_set = sorted(non_terminal.first_set)
-
+        non_terminal.follow_set = sorted(non_terminal.follow_set)
         # add epsilon to end if it contains one
         if "epsilon" in non_terminal.first_set:
             non_terminal.first_set.remove("epsilon")
             non_terminal.first_set.append("epsilon")
+        if "$" in non_terminal.follow_set:
+            non_terminal.follow_set.remove("$")
+            non_terminal.follow_set.append("$")
 
-    # print
-    print("First:")
+def print_sets(command):
+    print(command + ":")
     for key in sorted(non_terminals):
         non_terminal = non_terminals[key]
         print(" " + non_terminal.value + " ->", end="")
 
-        for val in non_terminal.first_set:
-            print(" " + val, end="")
+        if command == "First":
+            for val in non_terminal.first_set:
+                print(" " + val, end="")
+        else:
+            for val in non_terminal.follow_set:
+                print(" " + val, end="")
         print("")
 
 
@@ -95,6 +111,39 @@ def find_first_sets(non_terminal):
     non_terminal.first_set = symbols
 
 def find_follow_sets(non_terminal):
+
+
+    for grammar in grammars[non_terminal.value]:
+        for i in range(0, len(grammar.rhs)):
+            symbols = set()
+
+            # S -> X Y
+            symbol_x = grammar.rhs[i]
+            symbol_y = "epsilon"
+
+            # if its a terminal no need to find follow set
+            if symbol_x not in non_terminals:
+                continue
+            # if the index is at the end or
+            # T ::= F
+            elif i == len(grammar.rhs) - 1:
+                find_follow_sets(non_terminal)
+                symbols |= non_terminal.follow_set
+            else:
+                symbol_y = grammar.rhs[i+1]
+
+            if symbol_y in non_terminals:
+                symbols |= non_terminals[symbol_y].first_set
+
+                if "epsilon" in symbols:
+                    find_follow_sets(non_terminal)
+                    symbols |= non_terminal.follow_set
+                    symbols.remove("epsilon")
+            else:
+                if symbol_y != "epsilon":
+                    symbols.add(symbol_y)
+
+            non_terminals[symbol_x].follow_set |= symbols
 
 
 if __name__ == '__main__':
